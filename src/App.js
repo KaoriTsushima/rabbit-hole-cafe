b77f97d7baca4d3d205c9263beeeaba9852c47be
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-23 11:50:05 +0100

    created new cafe project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Wlecome to</h3>
+        <h1>Rabbit Hole Cafe</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
