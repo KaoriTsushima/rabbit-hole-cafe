b79adf360bc3ae96a622069618de5f3acaa76d0a
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-26 16:30:37 +0100

    fixed spelling mistake

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Wlecome to</h3>
+        <h3>Welcome to</h3>
         <h1>Rabbit Hole Cafe</h1>
       </header>
     </div>
