edf4e00a1e0c7e95747470d4ccd79820795334c8
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-27 10:51:09 +0100

    remove comment

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,18 +6,6 @@ import Navbar from "react-bootstrap/Navbar";
 export default function Header() {
   return (
     <>
-      {/* <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Rabbit Hole Cafe</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Menu</Nav.Link>
-            <Nav.Link href="#pricing">About us</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <br /> */}
-
       <Navbar bg="light" data-bs-theme="light" sticky="top">
         <Container>
           <Navbar.Brand href="/">Rabbit Hole Cafe</Navbar.Brand>
