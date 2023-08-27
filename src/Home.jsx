3a900af9bc46a9d497a086634bbebf2ee31e1329
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-27 11:34:12 +0100

    changed main font family and background img

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router";
 
 export default function Home() {
@@ -9,7 +9,9 @@ export default function Home() {
     <header className="App-header">
       <h3>Welcome to</h3>
       <h1>Rabbit Hole Caf&eacute;</h1>
-      <Button className="mt-5 p-3" onClick={() => navigate('menu')}>See Our Menu</Button>
- </header>
+      <Button className="mt-5 p-3" onClick={() => navigate("menu")}>
+        See Our Menu
+      </Button>
+    </header>
   );
 }
