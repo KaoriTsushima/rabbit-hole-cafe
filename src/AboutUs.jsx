554bdec9b2784cca25d2be3e39ae0f2d2cc88d5c
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-09-01 10:24:11 +0100

    added staff cat comment

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Bear from "./img/mastar-bear.jpeg";
-// import Cat from "./img/staff-cat.jpeg";
+import Cat from "./img/staff-cat.jpeg";
 import "./AboutUs.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -21,13 +21,29 @@ export default function AboutUs() {
             />
           </Col>
           <Col xs lg="8" md={{ span: 4, offset: 1 }}>
-            <p>
-              Hello! I’m Bearist, who is a master of the Rabbit Hole Café! Our
-              café is based on a home café bar. So when you visit here, let’s
-              enjoy our café and chatting with us! Also, we have two rabbits
-              (Kinako & Sesame). If you want to play with them, feel free to ask
-              me!
+            <p className="master-comment">
+              Hello!
+              <br />
+              I’m Bearista, who is a master of the Rabbit Hole Café! Our café is
+              based on a home café bar. So when you visit here, let’s enjoy our
+              café and chatting with us!
+              <br />
+              Also, we have two rabbits (Kinako & Sesame). If you want to play
+              with them, feel free to ask me!
             </p>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col xs lg="8" md={{ span: 4, offset: 1 }}>
+            <p className="staff-comment">
+              <br />
+              I'm waiting for your visit!
+            </p>
+          </Col>
+          <Col xs lg="2">
+            <img alt="staff-cat" src={Cat} width={100} className="staff-cat" />
           </Col>
         </Row>
       </Container>
